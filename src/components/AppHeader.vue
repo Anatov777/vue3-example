98bc789904add21dbb2a502b9cbc48d2e2168311
Anatov777 <anatovtimur@gmail.com>
2022-12-01 00:28:42 +0300

    add burger menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,45 +1,62 @@
 <script setup lang="ts">
 import useAuthUser from '@/composables/UseAuthUser';
+import BurgerMenu from '@/components/BurgerMenu.vue';
+
+import { ref, watch } from 'vue';
+import type { Ref } from 'vue';
+
+const isShowBurgerMenu: Ref<boolean> = ref(false);
+
+console.log(this)
+
 const { isLoggedIn } = useAuthUser();
 </script>
 <template>
   <header class="header">
-    <ul class="header__list">
-      <li class="header__list-item">
-        <router-link :to="{ name: 'Home' }" class="header__link">
-          Главная
-        </router-link>
-      </li>
-      <li class="header__list-item">
-        <router-link :to="{ name: 'Profile' }" class="header__link">
-          Профиль
-        </router-link>
-      </li>
-    </ul>
-    <ul v-if="isLoggedIn()" class="header__list">
-      <li class="header__list-item">
-        <router-link :to="{ name: 'Logout' }" class="header__link">
-          Выйти
-        </router-link>
-      </li>
-    </ul>
-    <ul v-else class="header__list">
-      <li class="header__list-item">
-        <router-link :to="{ name: 'Login' }" class="header__link">
-          Вход
-        </router-link>
-      </li>
-      <li class="header__list-item">
-        <router-link :to="{ name: 'Register' }" class="header__link">
-          Регистрация
-        </router-link>
-      </li>
-    </ul>
+    <nav class="header__menu">
+      <ul class="header__list">
+        <li v-if="isShowBurgerMenu" class="header__list-item">
+          <BurgerMenu :is-logged-in="isLoggedIn()" />
+        </li>
+        <li class="header__list-item">
+          <router-link :to="{ name: 'Home' }" class="header__link">
+            Главная
+          </router-link>
+        </li>
+        <li class="header__list-item">
+          <router-link :to="{ name: 'Profile' }" class="header__link">
+            Профиль
+          </router-link>
+        </li>
+      </ul>
+      <ul v-if="isLoggedIn()" class="header__list">
+        <li class="header__list-item">
+          <router-link :to="{ name: 'Logout' }" class="header__link">
+            Выйти
+          </router-link>
+        </li>
+      </ul>
+      <ul v-else class="header__list">
+        <li class="header__list-item">
+          <router-link :to="{ name: 'Login' }" class="header__link">
+            Вход
+          </router-link>
+        </li>
+        <li class="header__list-item">
+          <router-link :to="{ name: 'Register' }" class="header__link">
+            Регистрация
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 <style lang="postcss">
 .header {
-  @apply flex bg-gray-700 text-white justify-between;
+  @apply bg-gray-700 text-white;
+  &__menu {
+    @apply flex justify-between;
+  }
   &__list {
     @apply flex;
     &-item {
@@ -47,9 +64,11 @@ const { isLoggedIn } = useAuthUser();
     }
   }
   &__link {
-    @apply p-5;
     @apply hover:bg-gray-500;
-    @apply transition-all ease-in-out duration-300;
+    @apply transition-all ease-in-out duration-300 p-5;
+    display: flex;
+    align-items: center;
+    height: 100%;
   }
 }
 </style>
---
+++ src/components/BurgerMenu.vue
@@ -0,0 +1,173 @@
+<script setup lang="ts">
+import IconBurger from '@/components/icons/IconBurger.vue';
+import IconClose from '@/components/icons/IconClose.vue';
+
+import { ref, computed, onUnmounted } from 'vue';
+import type { Ref } from 'vue';
+
+interface Props {
+  isLoggedIn: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isLoggedIn: false,
+});
+
+const isShowBurgerMenu: Ref<boolean> = ref(false);
+const burgerMenu = ref(null);
+
+const burgerMenuItems = computed<any>(() => {
+  const items = [
+    {
+      label: 'burger-item-1',
+      to: 'Home',
+      text: 'Главная',
+      isShow: true,
+    },
+    {
+      label: 'burger-item-2',
+      to: 'Profile',
+      text: 'Профиль',
+      isShow: true,
+    },
+    {
+      label: 'burger-item-3',
+      to: 'Login',
+      text: 'Логин',
+      isShow: !props.isLoggedIn,
+    },
+    {
+      label: 'burger-item-4',
+      to: 'Register',
+      text: 'Регистрация',
+      isShow: !props.isLoggedIn,
+    },
+    {
+      label: 'burger-item-5',
+      to: 'Logout',
+      text: 'Выйти',
+      isShow: props.isLoggedIn,
+    },
+  ];
+  return items;
+});
+
+const toggleBurgerMenu = () => {
+  isShowBurgerMenu.value = !isShowBurgerMenu.value;
+};
+
+const clickListenerHandler = (event: any) => {
+  if (
+    isShowBurgerMenu.value &&
+    (event.target as HTMLElement).closest('.burger-menu') !== burgerMenu.value
+  ) {
+    toggleBurgerMenu();
+  }
+};
+
+document.body.addEventListener('click', clickListenerHandler);
+
+onUnmounted(() =>
+  document.body.removeEventListener('click', clickListenerHandler)
+);
+</script>
+<template>
+  <div class="burger-menu" ref="burgerMenu">
+    <div class="burger-menu__icon-wrap" @click="toggleBurgerMenu">
+      <IconBurger class="burger-menu__icon" />
+    </div>
+
+    <Transition name="slide-fade">
+      <div v-if="isShowBurgerMenu" class="burger-menu__menu">
+        <div class="burger-menu__wrap">
+          <div class="burger-menu__top">
+            <h2>Меню</h2>
+            <IconClose
+              class="burger-menu__icon-close"
+              @click="toggleBurgerMenu"
+            />
+          </div>
+          <div class="burger-menu__content">
+            <ul class="burger-menu__list">
+              <template v-for="item in burgerMenuItems" :key="item.label">
+                <li v-if="item.isShow" class="burger-menu__list-item">
+                  <router-link
+                    :to="{ name: item.to }"
+                    class="burger-menu__link"
+                    @click="toggleBurgerMenu"
+                  >
+                    {{ item.text }}
+                  </router-link>
+                </li>
+              </template>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </div>
+</template>
+<style lang="postcss">
+.burger-menu {
+  &__menu {
+    @apply fill-gray-200;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+  }
+
+  &__icon-wrap {
+    @apply cursor-pointer fill-gray-200 p-4;
+    height: 100%;
+    position: relative;
+    z-index: 1;
+    &:hover {
+      @apply bg-gray-500;
+    }
+  }
+  &__icon {
+    @apply cursor-pointer fill-gray-200;
+    height: 100%;
+  }
+  &__icon-close {
+    @apply cursor-pointer fill-gray-800;
+    width: 40px;
+    height: 40px;
+    &:hover {
+      @apply bg-gray-400 rounded-3xl;
+    }
+  }
+  &__wrap {
+    @apply bg-gray-200 text-gray-600 border-2 border-gray-400;
+    @apply rounded-r-3xl border-t-0 border-l-0 pb-5 pt-1;
+    width: 300px;
+    min-height: 300px;
+  }
+  &__top {
+    @apply flex justify-between items-center p-5;
+    font-size: 24px;
+    font-weight: bold;
+  }
+  &__link {
+    @apply hover:bg-gray-500 hover:text-white;
+    @apply transition-all ease-in-out duration-300 p-5;
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.3s ease;
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(-100%);
+  }
+}
+</style>
